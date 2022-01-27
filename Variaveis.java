2f31bb63c60dec2aa36f02e965e4391d0e272ca2
thaisNY <71556315+thaisNY@users.noreply.github.com>
2022-01-27 04:16:53 -0300

    variaveis
    variaveis

---
+++ Variaveis.java
@@ -0,0 +1,7 @@
+package facebook.aulaJava.variaveis; // a convenção é letra menuscula
+public class Variaveis {
+    public static void main(String[] args){
+        int idade = 27;
+        System.out.println("Thais tem" + idade + "anos");
+    }
+}
