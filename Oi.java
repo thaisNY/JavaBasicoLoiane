550d245796412f6dc421ec015f2b18f2425eda07
thaisNY <71556315+thaisNY@users.noreply.github.com>
2022-01-28 21:43:09 -0300

    char2
    lidando com char

---
+++ Oi.java
@@ -0,0 +1,11 @@
+public class Oi {
+    public static void main(String args[]) {
+        char o = 111;
+        char i = 105;
+        char interogacao = 0x3f;
+        System.out.println(o + i);
+        System.out.println("" + o + i + interogacao);
+    }
+}
+//116
+//Oi?
