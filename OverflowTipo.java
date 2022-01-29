99394e47bf947f900fcec26740b3c4a3365c56aa
thaisNY <71556315+thaisNY@users.noreply.github.com>
2022-01-28 23:47:24 -0300

    OverflowTipo
    OverflowTipo

---
+++ OverflowTipo.java
@@ -0,0 +1,8 @@
+public class OverflowTipo {
+    public static void main(String args[]) {
+      
+      int var1 = 2147483647;
+      int var2 = 1;
+      System.out.println(var1 + var2);
+    }
+}
