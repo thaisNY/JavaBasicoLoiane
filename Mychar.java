fbd0521ed4a2ccf7c4fd0d595a3674ee58210c73
thaisNY <71556315+thaisNY@users.noreply.github.com>
2022-01-28 21:32:15 -0300

    char
    char

---
+++ Mychar.java
@@ -0,0 +1,10 @@
+public class MyChar {
+    public static void main(String args[]) {
+        char o = 111;
+        char i = 105;
+        System.out.println(o + i);
+        System.out.println(" " + o + i);
+    }
+}
+//116
+//oi
